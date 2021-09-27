004cd856aba01e87b2fa619bb0e6b01d2fb9d1e1
Elvis Balija <ebalija@gmail.com>
2021-09-27 02:53:51 +0200

    Commit 1.2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var allQues = document.getElementsByClassName('question');
-var quesBetwen = document.getElementsByClassName('questions');
+var allQues = document.getElementsByClassName("question");
+var quesBetwen = document.getElementsByClassName("questions");
 for (i = 0; i < quesBetwen.length; i++) {
-    quesBetwen[i].addEventListener('click', removeClass);
+  quesBetwen[i].addEventListener("click", removeClass);
 }
 function removeClass() {
-    var changeClass = this.parentNode.className;
-    for (i = 0; i < allQues.length; i++) {
-        allQues[i].className = 'question close';
-    }
-    if (changeClass == 'question close') {
-        this.parentNode.className = 'question open';
-    }
+  var changeClass = this.parentNode.className;
+  for (i = 0; i < allQues.length; i++) {
+    allQues[i].className = "question close";
+  }
+  if (changeClass == "question close") {
+    this.parentNode.className = "question open";
+  }
 }
